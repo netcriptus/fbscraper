0554c45980bfbd6a90673fd2ec8eb9acc6788cc9
Fernando Cezar <fernando.cezar.bernardelli@orderbird.com>
2017-02-03 11:24:22 +0100

    log photo tags

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,7 @@
 require 'koala'
 require 'ruby-progressbar'
 
+# TODO: Don't use hardcoded tokens
 app_token = '1026238860854081|fvTV03tYC4zTMrZ-TbvZORzUjMg'
 @client = Koala::Facebook::API.new(app_token)
 filename = Time.now.getutc.to_s + '.csv'
@@ -12,7 +13,7 @@ end
 
 def photo_tags(post_id)
   begin
-    @client.get_connections(post_id, 'tags')
+    @client.get_connections(post_id, 'tags', {fields: ['id']})
   rescue
     # not all photos have tags resources.
     # Sometimes Facbeook will return [], sometimes raise an Exception.
@@ -30,15 +31,10 @@ end
 
 STDIN.each_line do |line|
   page_id, limit = line.strip.split(',')
-  progressbar = ProgressBar.create(format: 'Fetching posts %c/%C|%b>%i|', length: 80, total: limit.to_i)
+  progressbar = ProgressBar.create(format: 'Fetching posts %c/%C |%b>%i|', length: 80, total: limit.to_i)
 
   page_posts(page_id, limit).each do |post|
     progressbar.increment
-
-    if post['type'] == 'photo'
-      tags = photo_tags(post['id'].split('_')[1])
-    end
-
     common_info = "#{page_id},#{post['id'].split('_')[1]},#{post['type']}"
 
     post_reactions(post['id']).each do |reaction|
@@ -47,6 +43,12 @@ STDIN.each_line do |line|
 
     post_comments(post['id']).each do |comment|
       output.write("#{comment['from']['id']},#{common_info},comment\n")
+    end
+
+    if post['type'] == 'photo'
+      photo_tags(post['id'].split('_')[1]).each do |tag|
+        output.write("#{tag['id']},#{common_info},tag\n")
+      end
     end
   end
   progressbar.finish
